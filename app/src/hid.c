3975d4a55791c576901efc73eb9e032284e3a618
Joseph Garner <joseph.peter.garner@gmail.com>
2025-11-04 18:44:11 +1100

    formatting

--- app/src/hid.c
+++ app/src/hid.c
@@ -264,10 +264,6 @@ bool zmk_hid_is_pressed(uint32_t usage) {
     return false;
 }
 
-struct zmk_hid_keyboard_report *zmk_hid_get_keyboard_report() {
-    return &keyboard_report;
-}
+struct zmk_hid_keyboard_report *zmk_hid_get_keyboard_report() { return &keyboard_report; }
 
-struct zmk_hid_consumer_report *zmk_hid_get_consumer_report() {
-    return &consumer_report;
-}
+struct zmk_hid_consumer_report *zmk_hid_get_consumer_report() { return &consumer_report; }
